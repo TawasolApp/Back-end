ac8189229ef5e9fd7497ad94b01fe547c972a8de
ziadasar <ziadasar@gmail.com>
2025-03-19 03:30:24 +0200

    feat(profile): added get and update profile. update bio,headline,bio,location,industry,profile and cover photos, and delete profile picture

--- src/profiles/profiles.controller.ts
+++ src/profiles/profiles.controller.ts
@@ -1,15 +1,78 @@
-import { Body, Controller, Get, Post, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Patch,
+  Post,
+  Put,
+  UseGuards,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { ProfilesService } from './profiles.service';
-//import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { Types } from 'mongoose';
+import { UpdateProfileDto } from './dto/update-profile.dto';
+import { _ } from '@faker-js/faker/dist/airline-CBNP41sR';
 
 @Controller('profile')
 export class ProfilesController {
-    constructor(private profilesService:ProfilesService) {}
+  private _id: Types.ObjectId = new Types.ObjectId('67d970368674cc71ffc6a4f3');
 
-    //@UseGuards(jwt)
-    @Post()
-    @UsePipes(new ValidationPipe())
-    createProfile(@Body () createProfileDto) {
-        return this.profilesService.createProfile(createProfileDto);
-    }
+  constructor(private profilesService: ProfilesService) {}
+
+  @Post()
+  @UsePipes(new ValidationPipe())
+  createProfile(@Body() createProfileDto) {
+    return this.profilesService.createProfile(createProfileDto);
+  }
+
+  @Get()
+  @UsePipes(new ValidationPipe())
+  getProfile(@Body() user: { username: string }) {
+    return this.profilesService.getProfile(user.username);
+  }
+
+  @Put()
+  @UsePipes(new ValidationPipe())
+  updateProfile(@Body(ValidationPipe) updateProfileDto) {
+    return this.profilesService.updateProfile( updateProfileDto,this._id);
+  }
+
+  @Patch('headline')
+  updateHeadline(@Body() updateHeadlineDto: UpdateProfileDto) {
+    return this.profilesService.updateHeadline(updateHeadlineDto,this._id);
+  }
+
+  @Patch('bio')
+  updateBio(@Body() updateBioDto: UpdateProfileDto) {
+    return this.profilesService.updateBio(updateBioDto,this._id);
+  }
+
+  @Patch('location')
+  updateLocation(@Body() updateLocationDto: UpdateProfileDto) {
+    return this.profilesService.updateLocation(updateLocationDto,this._id);
+  }
+
+  @Patch('industry')
+  updateIndustry(@Body() updateIndustryDto: UpdateProfileDto) {
+    return this.profilesService.updateIndustry(updateIndustryDto,this._id);
+  }
+
+  @Post('profile-picture')
+ 
+  updateProfilePicture(@Body() updateIndustryDto: UpdateProfileDto) {
+    return this.profilesService.updateProfilePicture(updateIndustryDto,this._id);
+  }
+
+  @Delete('profile-picture')
+  deleteProfilePicture() {
+    return this.profilesService.deleteProfilePicture(this._id);
+  }
+
+  @Post('cover-photo')
+  
+  uploadCoverPhoto(@Body() updateIndustryDto: UpdateProfileDto,_id: Types.ObjectId) {
+    return this.profilesService.uploadCoverPhoto(updateIndustryDto,_id);
+  }
 }
--- src/profiles/profiles.service.ts
+++ src/profiles/profiles.service.ts
@@ -1,8 +1,10 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, Type } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Profile } from './infrastructure/database/profile.schema';
 import { Model } from 'mongoose';
 import { CreateProfileDto } from './dto/create-profile.dto';
+import { NotFoundException } from '@nestjs/common';
+import { UpdateProfileDto } from './dto/update-profile.dto';
 
 @Injectable()
 export class ProfilesService {
@@ -17,6 +19,124 @@ constructor(@InjectModel(Profile.name) private profileModel: Model<Profile>) {}
         const newProfile = new this.profileModel(createProfileDto);
         return newProfile.save();
     }
+
+    async getProfile(username: string) {
+        return this.profileModel.findOne({ username }).exec();
+    }
+
+    async updateProfile( updateProfileDto: UpdateProfileDto,_id) {
+        const updatedProfile = await this.profileModel.findOneAndUpdate(
+            { _id:_id }, // Find by username
+            { $set: updateProfileDto }, // Only update specified fields
+            { new: true, runValidators: true } // Return updated document & apply validators
+        ).exec();
+    
+        if (!updatedProfile) {
+            throw new NotFoundException(`Profile not found`);
+        }
+    
+        return updatedProfile;
+    }
+
+    async updateHeadline(updateHeadlineDto: UpdateProfileDto,_id) {
+        console.log("updateHeadlineDto")
+        const updatedProfile = await this.profileModel.findOneAndUpdate(
+            { _id:_id },
+            { $set: { headline: updateHeadlineDto.headline } },
+            { new: true, runValidators: true }
+        ).exec();
+    
+        if (!updatedProfile) {
+            throw new NotFoundException(`Profile not found`);
+        }
+    
+        return updatedProfile;
+    }
+
+    async updateBio(updateBioDto: UpdateProfileDto,_id) {
+        const updatedProfile = await this.profileModel.findOneAndUpdate(
+            { _id:_id },
+            { $set: { bio: updateBioDto.bio } },
+            { new: true, runValidators: true }
+        ).exec();
+    
+        if (!updatedProfile) {
+            throw new NotFoundException(`Profile not found`);
+        }
+    
+        return updatedProfile;
+    }
+
+    async updateLocation(updateLocationDto: UpdateProfileDto,_id) {
+        const updatedProfile = await this.profileModel.findOneAndUpdate(
+            { _id:_id },
+            { $set: { location: updateLocationDto.location } },
+            { new: true, runValidators: true }
+        ).exec();
+    
+        if (!updatedProfile) {
+            throw new NotFoundException(`Profile not found`);
+        }
+    
+        return updatedProfile;
+    }
+
+    async updateIndustry(updateIndustryDto: UpdateProfileDto,_id) {
+        const updatedProfile = await this.profileModel.findOneAndUpdate(
+            { _id:_id },
+            { $set: { industry: updateIndustryDto.industry } },
+            { new: true, runValidators: true }
+        ).exec();
+    
+        if (!updatedProfile) {
+            throw new NotFoundException(`Profile not found`);
+        }
+    
+        return updatedProfile;
+    }
+
+    async updateProfilePicture(updateProfilePictureDto: UpdateProfileDto,_id) {
+        const updatedProfile = await this.profileModel.findOneAndUpdate(
+            { _id:_id },
+            { $set: { profilePicture: updateProfilePictureDto.profile_picture } },
+            { new: true, runValidators: true }
+        ).exec();
+    
+        if (!updatedProfile) {
+            throw new NotFoundException(`Profile not found`);
+        }
+    
+        return updatedProfile;
+    }
+
+    async deleteProfilePicture(_id) {
+        const updatedProfile = await this.profileModel.findOneAndUpdate(
+            { _id:_id },
+            { $unset: { profilePicture: '' } },
+            { new: true, runValidators: true }
+        ).exec();
+    
+        if (!updatedProfile) {
+            throw new NotFoundException(`Profile not found`);
+        }
+    
+        return updatedProfile;
+    }
+
+    async uploadCoverPhoto(updateCoverPhotoDto: UpdateProfileDto, _id) {
+        const updatedProfile = await this.profileModel.findOneAndUpdate(
+            { _id:_id},
+            { $set: { coverPhoto: updateCoverPhotoDto.cover_photo } },
+            { new: true, runValidators: true }
+        ).exec();
+    
+        if (!updatedProfile) {
+            throw new NotFoundException(`Profile not found`);
+        }
+    
+        return updatedProfile;
+    }
+    
     
 }
 
