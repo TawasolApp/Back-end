c8f5dd5f044cbf5df6c08ca4fc5bf33946f101ff
ziadasar <ziadasar@gmail.com>
2025-03-20 14:22:16 +0200

    refactor(UpdateProfileDto): added attributes and examples for update dto

--- src/profiles/dto/update-profile.dto.ts
+++ src/profiles/dto/update-profile.dto.ts
@@ -1,6 +1,45 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateProfileDto } from './create-profile.dto';
-export class UpdateProfileDto extends PartialType(CreateProfileDto) {
 
+import {  IsOptional, IsString } from 'class-validator';
+import {  ApiPropertyOptional } from '@nestjs/swagger';
+export class UpdateProfileDto {
+    @ApiPropertyOptional({ description: 'Username', example: 'johndoe' })
+    @IsOptional()
+    @IsString()
+    username: string;
+  
+    @ApiPropertyOptional({ description: 'Profile picture URL', example: 'https://example.com/profile.jpg' })
+    @IsOptional()
+    @IsString()
+    profile_picture?: string;
+  
+    @ApiPropertyOptional({ description: 'Cover photo URL', example: 'https://example.com/cover.jpg' })
+    @IsOptional()
+    @IsString()
+    cover_photo?: string;
+  
+    @ApiPropertyOptional({ description: 'Resume URL', example: 'https://example.com/resume.pdf' })
+    @IsOptional()
+    @IsString()
+    resume?: string;
+  
+    @ApiPropertyOptional({ description: 'Headline', example: 'Software Engineer at Google' })
+    @IsOptional()
+    @IsString()
+    headline?: string;
+  
+    @ApiPropertyOptional({ description: 'Bio', example: 'Passionate about AI and cloud computing' })
+    @IsOptional()
+    @IsString()
+    bio?: string;
+  
+    @ApiPropertyOptional({ description: 'Location', example: 'New York, USA' })
+    @IsOptional()
+    @IsString()
+    location?: string;
+  
+    @ApiPropertyOptional({ description: 'Industry', example: 'Computer Software' })
+    @IsOptional()
+    @IsString()
+    industry?: string;
     
 }
