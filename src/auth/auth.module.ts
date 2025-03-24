a2fc9a58b93b2353f92b931da934f7d781e954b4
Maryammhabebb <maryamhabeb6803@gmail.com>
2025-03-24 22:18:11 +0200

    fix(auth):made the secret key dependent on the .env file

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -17,7 +17,7 @@ import { User, UserSchema } from '../users/infrastructure/database/user.schema';
       { name: User.name, schema: UserSchema }, 
     ]),
     JwtModule.register({
-      secret: process.env.JWT_SECRET || '4a52519e47d98ddd4b515a71ca31443d530b16bd48218cacd2805ea7d0cdc5d4',
+      secret: process.env.JWT_SECRET ,
       signOptions: { expiresIn: '1h' },
     }),
   ],
