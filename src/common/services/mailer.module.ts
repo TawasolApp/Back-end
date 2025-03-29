087909c829f5d9044a03b91d4234e105db7d4189
NoorAlHadidi <noor.ahmed.alhadidi@gmail.com>
2025-03-30 01:37:04 +0200

    chore(merge): merged all infrastructure folders and module files from develop into feature/companies

--- src/common/services/mailer.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MailerService } from './mailer.service';
-
-@Module({
-  providers: [MailerService],
-  exports: [MailerService], // <-- allows other modules to use it
-})
-export class MailerModule {}
--- src/connections/dtos/create-request.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsMongoId, IsNotEmpty } from 'class-validator';
-
-export class CreateRequestDto {
-  @IsMongoId()
-  @IsNotEmpty()
-  readonly userId: string;
-}
