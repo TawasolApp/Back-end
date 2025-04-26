4b5aef0f228beb6a8a9c77bb3b69f2e58926e2f8
ziadasar <ziadasar@gmail.com>
2025-04-26 19:46:37 +0300

    feat(messages): messages are sent and saved in database also conversations are saved and last message id is updated

--- src/messages/infrastructure/database/schemas/conversation.schema.ts
+++ src/messages/infrastructure/database/schemas/conversation.schema.ts
@@ -8,13 +8,17 @@ export type ConversationDocument = Conversation & Document;
   versionKey: false,
 })
 export class Conversation {
-  @Prop({ type: Types.ObjectId, auto: true })
+  @Prop({
+    type: Types.ObjectId,
+    auto: true,
+    default: () => new Types.ObjectId(),
+  })
   _id: Types.ObjectId;
 
   @Prop({ type: [{ type: Types.ObjectId, ref: 'User' }], required: true })
   participants: Types.ObjectId[];
 
-  @Prop({ type: Types.ObjectId, ref: 'Message'})
+  @Prop({ type: Types.ObjectId, ref: 'Message' })
   last_message_id: Types.ObjectId;
 
   @Prop()
--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,4 +1,54 @@
 import { Injectable } from '@nestjs/common';
-
+import { InjectModel } from '@nestjs/mongoose';
+import {
+  Message,
+  MessageDocument,
+} from './infrastructure/database/schemas/message.schema';
+import { Model, Types } from 'mongoose';
+import {
+  Conversation,
+  ConversationDocument,
+} from './infrastructure/database/schemas/conversation.schema';
+import { MessageStatus } from './enums/message-status.enum';
 @Injectable()
-export class MessagesService {}
+export class MessagesService {
+  constructor(
+    @InjectModel(Message.name)
+    private readonly messageModel: Model<MessageDocument>,
+    @InjectModel(Conversation.name)
+    private readonly conversationModel: Model<ConversationDocument>,
+  ) {}
+
+  async createMessage(
+    senderId: string,
+    receiverId: string,
+    messageText: string,
+    messageDate: Date,
+  ) {
+    let conversation = await this.conversationModel.findOne({
+      participants: { $all: [senderId, receiverId] },
+    });
+
+    if (!conversation) {
+      conversation = await this.conversationModel.create({
+        participants: [senderId, receiverId],
+        unseen_count: 0,
+      });
+    }
+
+    const newMessage = await this.messageModel.create({
+      _id: new Types.ObjectId(),
+      sender_id: senderId,
+      conversation_id: conversation._id,
+      text: messageText,
+      status: MessageStatus.Sent,
+      sent_at: messageDate,
+    });
+
+    conversation.last_message_id = newMessage._id;
+    conversation.unseen_count += 1;
+    await conversation.save();
+
+    return { conversation, message: newMessage };
+  }
+}
