87c26cd678f0340139527da52f4e9b6cf5a25473
ziadasar <ziadasar@gmail.com>
2025-04-29 16:21:56 +0300

    init

--- src/security/security.module.ts
+++ src/security/security.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { SecurityController } from './security.controller';
+import { SecurityService } from './security.service';
 
-@Module({})
+@Module({
+  controllers: [SecurityController],
+  providers: [SecurityService]
+})
 export class SecurityModule {}
